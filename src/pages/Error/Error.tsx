92bfc24130dafd954098fae2f55fdc2395470456
pmochalov <52402489+pmochalov@users.noreply.github.com>
2025-02-06 20:41:41 +0300

    Добавлена страница обработки ошибки

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,20 +1,34 @@
-import * as React from "react";
-import { useRouteError } from "react-router-dom";
+import { useRouteError } from "react-router";
+// import * as React from "react";
+
+// import "./Layout.css";
+
+import { H1 } from "../../shared/ui/H1/H1";
+import { Footer } from "../../widgets/ui/Footer";
+import { Header } from "../../widgets/ui/Header";
 
 type ErrorType = {
-    status?: number;
     statusText?: string;
-    message?: string;
+    data?: string;
 };
 
 const ErrorPage: React.FC = () => {
-    let error = useRouteError() as ErrorType;
+    const error = useRouteError() as ErrorType;
 
     return (
-        <div>
-            <h1>Ошибка</h1>
-            <p>Статус: {error.status}</p>
-            <p>Сообщение: {error.statusText || error.message}</p>
+        <div className='grid grid-cols-1 grid-template-rows-[auto_1fr_auto] min-h-full'>
+            <Header />
+
+            <div className='container mx-auto bg-white'>
+                <main className='p-4'>
+                    <div className='flex flex-col gap-5'>
+                        <H1 title={`${error.statusText}`} />
+                        <p>{error.data}</p>
+                    </div>
+                </main>
+            </div>
+
+            <Footer />
         </div>
     );
 };
